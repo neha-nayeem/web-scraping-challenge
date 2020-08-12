312db1db72fa131e3e190aba59a96902d65afd18
neha-nayeem <61065609+neha-nayeem@users.noreply.github.com>
2020-08-12 03:07:59 -0400

    Changed browser headless to True for background scraping

--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -8,7 +8,7 @@ def init_browser():
     # @NOTE: Replace the path with your actual path to the chromedriver
     #For mac users
     executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
-    return Browser("chrome", **executable_path, headless=False)
+    return Browser("chrome", **executable_path, headless=True)
 
     #For windows users
     # executable_path = {'executable_path': 'driver/chromedriver.exe'}
